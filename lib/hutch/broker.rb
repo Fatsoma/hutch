6ff8090fa9becc3ed76fd613a7e58434ab6ad1a6
Bill Ruddock <bill.ruddock@gmail.com>
2015-09-12 08:31:29 +0100

    Moved handlers to lib/hutch/broker_handlers.rb

--- lib/hutch/broker.rb
+++ lib/hutch/broker.rb
@@ -362,40 +362,6 @@ module Hutch
       "#{scheme}://#{p[:username]}@#{p[:host]}:#{p[:port]}/#{p[:vhost].sub(%r{^/}, '')}"
     end
 
-    def with_authentication_error_handler
-      yield
-    rescue Net::HTTPServerException => ex
-      logger.error "HTTP API connection error: #{ex.message.downcase}"
-      if ex.response.code == '401'
-        raise AuthenticationError, 'invalid HTTP API credentials'
-      else
-        raise
-      end
-    end
-
-    def with_connection_error_handler
-      yield
-    rescue Errno::ECONNREFUSED => ex
-      logger.error "HTTP API connection error: #{ex.message.downcase}"
-      raise ConnectionError, "couldn't connect to HTTP API at #{api_config.sanitized_uri}"
-    end
-
-    def with_bunny_precondition_handler(item)
-      yield
-    rescue Hutch::Adapter::PreconditionFailed => ex
-      logger.error ex.message
-      s = "RabbitMQ responded with 406 Precondition Failed when creating this #{item}. " \
-          'Perhaps it is being redeclared with non-matching attributes'
-      raise WorkerSetupError, s
-    end
-
-    def with_bunny_connection_handler(uri)
-      yield
-    rescue Hutch::Adapter::ConnectionRefused => ex
-      logger.error "amqp connection error: #{ex.message.downcase}"
-      raise ConnectionError, "couldn't connect to rabbitmq at #{uri}. Check your configuration, network connectivity and RabbitMQ logs."
-    end
-
     def work_pool_threads
       channel_work_pool.threads || []
     end
