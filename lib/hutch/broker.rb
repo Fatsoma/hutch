4384c052e1c19b2850639b78f81ab99d4351b1be
Andy Whittle <andy.whittle@fatsoma.com>
2022-10-13 16:45:56 +0100

    Update channel broker declare exchange from upstream broker
    The upstream has updated the declare exchange logic in the broker class, this needs migrating to the channel broker

--- lib/hutch/broker.rb
+++ lib/hutch/broker.rb
@@ -125,14 +125,7 @@ module Hutch
     end
 
     def declare_exchange(ch = channel)
-      exchange_name = @config[:mq_exchange]
-      exchange_type = @config[:mq_exchange_type]
-      exchange_options = { durable: true }.merge(@config[:mq_exchange_options])
-      logger.info "using topic exchange '#{exchange_name}'"
-
-      with_bunny_precondition_handler('exchange') do
-        Adapter.new_exchange(ch, exchange_type, exchange_name, exchange_options)
-      end
+      channel_broker.declare_exchange(ch)
     end
 
     def declare_exchange!(*args)
